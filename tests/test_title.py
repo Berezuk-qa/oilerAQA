9c4dd40a7f0d0367643a30164ae21237504e86d2
qa <bereziuk.o@i.ua>
2024-09-13 19:58:57 +0300

    Test day 2

--- tests/test_title.py
+++ tests/test_title.py
@@ -58,6 +58,23 @@ def test_title_blog_ua(page: Page):
     page.goto("https://oiler.pro/ua-ua/blog/")
     expect(page).to_have_title(re.compile("Корисні статті та поради щодо ремонту та обслуговування автомобілів"))
 
+def test_title_zapchasti_bmw_5_ua(page: Page):
+    page.goto("https://oiler.pro/ua-ua/zapchasti/bmw/5-touring-g31/category-podveska---amortizacija_1/filter/brand-sachs/")
+    expect(page).to_have_title(re.compile("Підвіска / амортизація SACHS для BMW 5 Touring"))
+
+def test_title_maslo_3filtra_ua(page: Page):
+    page.goto("https://oiler.pro/ua-ua/avtotovary/masla/motornye-masla/filter/brand-motul/viscosity-5w-40/specification-acea-b4/")
+    expect(page).to_have_title(re.compile("Моторні масла Motul 5w-40 B4"))
+
+def test_title_maslo_2filtra_ua(page: Page):
+    page.goto("https://oiler.pro/ua-ua/avtotovary/masla/motornye-masla/motornye-masla-vo-lvove/filter/brand-motul/")
+    expect(page).to_have_title(re.compile("Моторні масла у Львові Motul"))
+
+
+
+
+
+
 # Перевірка тайтлів для російськомовної версії сайту
 
 def test_title_main_ru(page: Page):
@@ -116,3 +133,16 @@ def test_title_sto_ru(page: Page):
 def test_title_blog_ru(page: Page):
     page.goto("https://oiler.pro/ua-ru/blog/")
     expect(page).to_have_title(re.compile("Полезные статьи и советы по ремонту и обслуживанию автомобилей"))
+
+
+def test_title_zapchasti_bmw_5_ru(page: Page):
+    page.goto("https://oiler.pro/ua-ru/zapchasti/bmw/5-touring-g31/category-podveska---amortizacija_1/filter/brand-sachs/")
+    expect(page).to_have_title(re.compile("Подвеска / амортизация SACHS для BMW 5 Touring"))
+
+def test_title_maslo_3filtra_ru(page: Page):
+    page.goto("https://oiler.pro/ua-ru/avtotovary/masla/motornye-masla/filter/brand-motul/viscosity-5w-40/specification-acea-b4/")
+    expect(page).to_have_title(re.compile("Моторные масла Motul 5w-40 B4"))
+
+def test_title_maslo_2filtra_ru(page: Page):
+    page.goto("https://oiler.pro/ua-ru/avtotovary/masla/motornye-masla/motornye-masla-vo-lvove/filter/brand-motul/")
+    expect(page).to_have_title(re.compile("Моторные масла во Львове Motul"))
